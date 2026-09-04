763e3fa735cc910cd2b017f1f34aa51f540a8fb4
DhanyashreePS <dhanyaps888@gmail.com>
2026-09-04 18:01:42 +0530

    Completed optional prameters in constructors.

--- lesson1.py
+++ lesson1.py
@@ -1,14 +1,17 @@
 class Human:
-    def __init__(self,name, age):
+    def __init__(self,name="Unknowm", age=0, salary=-1):
         print("Const called by: ",name)
-        self.name=name
+        self.name=name #it is instance variable
         self.age=age
+        self.salary=salary
         
     def walk(self):
         print(f'{self.name} is walking')
 
 #Init for just passing value directly to class
-d=Human("Dhanya",20)
-p=Human("Preeti",22)
+d=Human("Dhanya",20,50000)
+p=Human("Preeti",2)
+alice=Human()
 d.walk()
+alice.walk()
 
