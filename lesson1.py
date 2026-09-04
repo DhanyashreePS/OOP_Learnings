2879cb975d26bbb257b59323eef41c7d0f8b7907
DhanyashreePS <dhanyaps888@gmail.com>
2026-09-04 17:52:25 +0530

    Completed lesson one with Human class instance.

--- lesson1.py
+++ lesson1.py
@@ -1,1 +1,14 @@
+class Human:
+    def __init__(self,name, age):
+        print("Const called by: ",name)
+        self.name=name
+        self.age=age
+        
+    def walk(self):
+        print(f'{self.name} is walking')
+
+#Init for just passing value directly to class
+d=Human("Dhanya",20)
+p=Human("Preeti",22)
+d.walk()
 
